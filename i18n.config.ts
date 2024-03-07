067ca9ea1b665b02f28b5a33877bd40b744f60ca
dxlliv <hello@dxlliv.dev>
2024-03-07 18:59:50 +0100

    Update to plxnner 0.0.1-alpha.4

--- i18n.config.ts
+++ i18n.config.ts
@@ -1,16 +1,5 @@
-import messages from 'plxnner/locales'
-
 export default defineI18nConfig(() => {
-    const plannerConfig = usePlannerConfig()
+    const appConfig = useAppConfig()
 
-    return {
-        locale: plannerConfig.locale,
-        fallbackLocale: plannerConfig.locale,
-        availableLocales: [
-          "en",
-          "it"
-        ],
-        messages,
-        warnHtmlInMessage: false,
-    }
+    return appConfig.i18n
 })
